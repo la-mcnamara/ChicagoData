56f3d46d7210b4c099d2d930e29d07dd7c99e8cd
lmcnamar <lauren.mcnamara@gmail.com>
2021-04-09 18:16:22 -0500

    data cleaning

--- explore_chi_data.py
+++ explore_chi_data.py
@@ -32,29 +32,26 @@ pyellow = '#ffa600'
 pgray = '#64666B'
 
 ##############  Get Data  ##############
-# This dataset shows monthly averages, by day type (weekday, Saturday or Sunday/Holiday) 
-# and monthly totals for all CTA bus routes, back to 2001.
-# bus_monthly_avg = pd.read_json("https://data.cityofchicago.org/resource/bynn-gwxy.json")
-# bus_monthly_avg.head()
-
 # Current Employee Names, Salaries, and Position Titles
-employee_salary = pd.read_json("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
-employee_salary.head()
+# employee_salary = pd.read_json("https://data.cityofchicago.org/resource/xzkq-xp2w.json")
+# employee_salary.head()
 # this method limits to 1000 rows
 
+# this method allows more data to be pulled
 with Socrata("data.cityofchicago.org", None) as client:
     results = client.get("xzkq-xp2w", limit=2000)
     employee_salary = pd.DataFrame.from_records(results)
     # need to get an app_token or suffer strict throttling limits
 
-##############  Clean Data  ##############
+##############  Clean Employee Salary Data  ##############
+#####  examine and clean name
 # count number of commas in name
 employee_salary['n_comma'] = employee_salary.name.str.count(',')
 freqit.oneway(employee_salary['n_comma']).freqtable()
 # 2000 recs all have one comma per name
 
 # split into first and last name
-employee_salary = employee_salary.merge(
+esalary = employee_salary.merge(
                     employee_salary.name.str.split(',',n=1, expand=True)
                     ,left_index=True
                     ,right_index=True
@@ -63,14 +60,66 @@ employee_salary = employee_salary.merge(
                                     ,1:'name_first'})
 
 # remove leading and trailing spaces from first name
-employee_salary['name_first'] = employee_salary.name_first.str.strip()
+esalary['name_first'] = esalary.name_first.str.strip()
 
-
-
+# split out middle initial
+esalary = esalary.merge(
+                    esalary.name_first.str.split(' ',n=1, expand=True)
+                    ,left_index=True
+                    ,right_index=True
+                    ,how='outer')   \
+                    .drop(columns=['name_first'])   \
+                    .rename(columns={0:'name_first'
+                                    ,1:'name_mid'})
 
 # count non-alpha characters
-not_alpha_name = employee_salary.loc[~(employee_salary.name_first.str.isalpha())
+not_alpha_name = esalary.loc[~(esalary.name_first.str.isalpha())
                                     ,'name_first']
 not_alpha_name
-employee_salary.head()
+# have one name with a hyphen
+
+
+#####  examine and clean salary
+
+# why is typical hours an object
+freqit.oneway(esalary['typical_hours']).freqtable()
+
+# convert numeric variables from object to float
+esalary['typical_hours'] = pd.to_numeric(esalary['typical_hours'], errors='coerce')
+esalary['hourly_rate'] = pd.to_numeric(esalary['hourly_rate'], errors='coerce')
+esalary['annual_salary'] = pd.to_numeric(esalary['annual_salary'], errors='coerce')
+
+# are salary vs hourly full or part time
+esalary.groupby(['full_or_part_time','salary_or_hourly']).size()
+# in sample set, salary is always full time and hourly can be
+# either full or part time
+
+# if hourly full time, is typical hours always 40
+esalary.loc[(esalary.full_or_part_time == 'F') & 
+            (esalary.salary_or_hourly == 'Hourly'),'typical_hours'].describe()
+# hours are generally 40 but not always
+
+# typical_hours are either 10, 20, 35, or 40 which per data documentation these
+#   are more categories than an actual number of hours, and employment may
+#   be seasonal and not year-round.  Better to convert annual to an 
+#   hourly estimate than calc an annual salary from hourly.
+
+# set hourly salary as annual salary / 2080 (2080 = 40 hrs x 52 wks)
+#   if annual salary not missing, otherwise use hourly_rate
+esalary['hourly_salary'] = esalary['hourly_rate']
+esalary.loc[~(esalary.annual_salary.isnull()),'hourly_salary'] = esalary['annual_salary']/2080
+
+esalary.info()
+esalary.head()
+
+##############  Exploratory Data Analysis  ##############
+# fields of interest:
+# controls: 
+#   job_titles
+#   department
+#   full_or_part_time
+#   salary_or_hourly
+# key fields:
+#   name_first - use to predict gender
+#   hourly_salary
 
